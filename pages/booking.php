5535f074f39e19333e96afdf3862625ac95027d1
Dung Nguyen <149448159+GatGuGamingGear@users.noreply.github.com>
2024-04-22 23:12:20 +0700

    update booking

--- pages/booking.php
+++ pages/booking.php
@@ -10,14 +10,14 @@
     <title>Hiroto | Template</title>
 
     <!-- Css Styles -->
-    <link rel='stylesheet' href='css/bootstrap.min.css' type='text/css'>
-    <link rel='stylesheet' href='css/font-awesome.min.css' type='text/css'>
-    <link rel='stylesheet' href='css/elegant-icons.css' type='text/css'>
-    <link rel='stylesheet' href='css/nice-select.css' type='text/css'>
-    <link rel='stylesheet' href='css/jquery-ui.min.css' type='text/css'>
-    <link rel='stylesheet' href='css/owl.carousel.min.css' type='text/css'>
-    <link rel='stylesheet' href='css/slicknav.min.css' type='text/css'>
-    <link rel='stylesheet' href='css/style.css' type='text/css'>
+    <link rel='stylesheet' href='../css/bootstrap.min.css' type='text/css'>
+    <link rel='stylesheet' href='../css/font-awesome.min.css' type='text/css'>
+    <link rel='stylesheet' href='../css/elegant-icons.css' type='text/css'>
+    <link rel='stylesheet' href='../css/nice-select.css' type='text/css'>
+    <link rel='stylesheet' href='../css/jquery-ui.min.css' type='text/css'>
+    <link rel='stylesheet' href='../css/owl.carousel.min.css' type='text/css'>
+    <link rel='stylesheet' href='../css/slicknav.min.css' type='text/css'>
+    <link rel='stylesheet' href='../css/style.css' type='text/css'>
 </head>
 
 <body>
@@ -30,7 +30,7 @@
     <div class='offcanvas-menu-overlay'></div>
     <div class='offcanvas-menu-wrapper'>
         <div class='offcanvas__logo'>
-            <a href='./index.php'><img src='img/logo.png' alt=''></a>
+            <a href='../index.php'><img src='../img/logo.png' alt=''></a>
         </div>
         <div id='mobile-menu-wrap'></div>
         <div class='offcanvas__btn__widget'>
@@ -45,7 +45,7 @@
         
         <div class='offcanvas__auth'>
             <ul>
-               <li><a href='./pages/login.html'>Login | Register</a></li>
+               <li><a href='../pages/login.html'>Login | Register</a></li>
             </ul>
         </div>
     </div>
@@ -66,7 +66,7 @@
                         <div class='header__top__right'>
                             <div class='header__top__auth'>
                                 <ul>
-                                 <li><a href='./pages/login.html'>Login | Register</a></li>
+                                 <li><a href='./login.html'>Login | Register</a></li>
                                 </ul>
                             </div>
                             
@@ -80,14 +80,14 @@
                 <div class='row'>
                     <div class='col-lg-2'>
                         <div class='header__logo'>
-                            <a href='./index.php'><img src='img/logo2.png' alt=''></a>
+                            <a href='../index.php'><img src='../img/logo2.png' alt=''></a>
                         </div>
                     </div>
                     <div class='col-lg-10'>
                         <div class='header__nav'>
                             <nav class='header__menu'>
                                 <ul class='menu__class'>
-                                    <li class='active'><a href='./index.php'>Home</a></li>
+                                    <li class='active'><a href='../index.php'>Home</a></li>
                                     <li><a href='./rooms.php'>Rooms</a></li>
                                     <li><a href='./about.php'>About Us</a></li>
                                     <li><a href='./blog-details.php'>Blog Details</a></li>  
@@ -308,13 +308,13 @@
     <!-- Footer Section End -->
 
     <!-- Js Plugins -->
-    <script src='js/jquery-3.3.1.min.js'></script>
-    <script src='js/bootstrap.min.js'></script>
-    <script src='js/jquery.nice-select.min.js'></script>
-    <script src='js/jquery-ui.min.js'></script>
-    <script src='js/jquery.slicknav.js'></script>
-    <script src='js/owl.carousel.min.js'></script>
-    <script src='js/main.js'></script>
+    <script src='../js/jquery-3.3.1.min.js'></script>
+    <script src='../js/bootstrap.min.js'></script>
+    <script src='../js/jquery.nice-select.min.js'></script>
+    <script src='../js/jquery-ui.min.js'></script>
+    <script src='../js/jquery.slicknav.js'></script>
+    <script src='../js/owl.carousel.min.js'></script>
+    <script src='../js/main.js'></script>
 </body>
 
 </html>
