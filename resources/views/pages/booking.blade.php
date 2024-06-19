a9330fe242bb4bd01f8046e58d45dfbe78e8a1bc
Peab2003 <149892005+Peab2003@users.noreply.github.com>
2024-06-19 16:40:05 +0700

    Update booking.blade.php

--- resources/views/pages/booking.blade.php
+++ resources/views/pages/booking.blade.php
@@ -18,8 +18,8 @@
                 <div
                     class="col-md-12 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0 text-center fh5co-table">
                     <div class="fh5co-intro fh5co-table-cell">
-                        <h1 class="text-center">ĐẶT PHÒNG CÙNG CHÚNG TÔI</h1>
-                        <p>Khách sạn GALAXYNG hân hạnh được phục vụ bạn</p>
+                        <h1 class="text-center">ĐẶT LỊCH XEM CĂN HỘ VỚI CHÚNG TÔI</h1>
+                        <p>BĐS GALAXYNG hân hạnh được phục vụ bạn</p>
                     </div>
                 </div>
             </div>
@@ -32,7 +32,7 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="section-title text-center" style="margin-bottom: 0rem">
-                            <h1>DANH SÁCH PHÒNG</h1>
+                            <h1>DANH SÁCH CĂN HỘ</h1>
                         </div>
                     </div>
                     <div id="availability" style="display: flex;
@@ -40,26 +40,18 @@
                                         align-items: center width: 100%;
                                         background-color: white">
                         <div class="a-col">
-                            Chọn loại phòng
+                            Chọn loại căn hộ
                             <select name="" id="" class="form-control"
                                 style="color: #db4118; font-weight: 16px; font-weight: 700;"
                                 onchange="changeLoaiPhong(this.value)">
-                                <option value="" disabled selected>Chọn loại phòng</option>
+                                <option value="" disabled selected>Chọn loại căn hộ</option>
                                 @foreach ($loaiphong as $lp)
                                     <option value="{{ $lp->id }}">{{ $lp->tenloaiphong }}</option>
                                 @endforeach
                                 <option value="all">Tất cả</option>
                             </select>
                         </div>
-                        <div class="a-col" id="sort">
-                            Sắp xếp
-                            <section>
-                                <select class="form-control" id="sort-phong" name="sort-phong"
-                                    style="color: #db4118; font-weight: 16px; font-weight: 700;">
-                                    <option value="" disabled selected>Sắp xếp theo</option>
-                                </select>
-                            </section>
-                        </div>
+                        
                     </div>
                 </div>
             </div>
@@ -68,7 +60,7 @@
         <div id="fh5co-hotel-section" style="padding: 4rem">
             <div class="container">
                 <div class="row" id="list-phong">
-                    {{-- Hiển thị phòng theo slect loại phòng
+                    {{-- Hiển thị căn hộ theo slect loại căn hộ
                     --}}
                     @if (isset($phong))
                         @foreach ($phong as $item)
@@ -81,7 +73,7 @@
                                         <a class="book-now text-center"
                                             href="{{ route('cart.addCart', ['id' => $item->id]) }}"
                                             onclick="addCartBooking($item->id)" data-id="{{ $item->id }}"><i
-                                                class="ti-calendar"></i> Book Now</a>
+                                                class="ti-calendar"></i> LIÊN HỆ</a>
                                     </div>
                                     <div class="desc">
                                         <h3><a>{{ $item->tenphong }}</a></h3>
